25c3af9400d5767d1b4918186afba994b82e797d
a-forghani-y79 <a.forghani.y79@gmail.com>
2020-04-09 03:14:39 +0430

    Initial commit

---
+++ src/sample/Task.java
@@ -0,0 +1,19 @@
+package sample;
+
+public class Task implements Runnable {
+
+    private String fileName="";
+    private String url = "";
+    Task(String url){
+
+    }
+
+
+
+
+
+    @Override
+    public void run() {
+
+    }
+}
